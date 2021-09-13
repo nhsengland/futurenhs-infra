4a3ec164968a9e73f2962e6672b6ae786f00cc7c
Richard <richard.ashman@cds.co.uk>
2021-09-13 15:36:18 +0100

    Reenabling TLS cert on App Gateway

--- src/dev/key-vault/outputs.tf
+++ src/dev/key-vault/outputs.tf
@@ -4,10 +4,10 @@ output "key_vault_id" {
 
 # Returns the versionless identifier for the certificate.  REmoving the version means any certificate
 # rotation in key vault should be automatically picked up by the application gateway with 24 hours
-#output key_vault_certificate_https_versionless_secret_id {
-#  value = trimsuffix(azurerm_key_vault_certificate.app_forum_https.secret_id, "${azurerm_key_vault_certificate.app_forum_https.version}")
-#}
+output key_vault_certificate_https_versionless_secret_id {
+  value = trimsuffix(azurerm_key_vault_certificate.app_forum_https.secret_id, "${azurerm_key_vault_certificate.app_forum_https.version}")
+}
 
-#output key_vault_certificate_https_name {
-#  value = azurerm_key_vault_certificate.app_forum_https.name
-#}
+output key_vault_certificate_https_name {
+  value = azurerm_key_vault_certificate.app_forum_https.name
+}
