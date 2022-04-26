b445058ad4fd627303d3972ab80815ff90f16c4c
RiccardoFarrugia <102971105+RiccardoFarrugia@users.noreply.github.com>
2022-04-26 16:21:17 +0100

    Update main.tf

--- src/dev/main.tf
+++ src/dev/main.tf
@@ -374,6 +374,7 @@ module "databases" {
     
 resource "azurerm_mssql_database" "forum" {
   resourceId                                                              ="/subscriptions/d9c0a8f2-3fae-48e7-a2dd-6ffcc5c1c994/resourceGroups/rg-fnhso-dev-uksouth-001/providers/Microsoft.Sql/servers/sql-fnhso-dev-uksouth-primary/databases/sqldb-fnhso-dev-uksouth-forum"
+  api_forum_application_shared_secret                                     = "${terraform.api_forum_application_shared_secret}"
   #api_forum_application_shared_secret                                     = $(api_forum_application_shared_secret)
  # api_govnotify_api_key                                                   = $(api_govnotify_api_key)
   #api_govnotify_registration_template_id                                  = $(api_govnotify_registration_template_id)
