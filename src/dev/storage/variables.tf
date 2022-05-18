95a5aed01cd7bccf60b584bcb52ca883d3c404ee
RiccardoFarrugia <102971105+RiccardoFarrugia@users.noreply.github.com>
2022-05-18 09:13:41 +0100

    Update variables.tf

--- src/dev/storage/variables.tf
+++ src/dev/storage/variables.tf
@@ -11,3 +11,14 @@ variable key_vault_id { type = string }
 variable log_analytics_workspace_resource_id { type = string }
 
 variable log_storage_account_id { type = string }
+variable "name" {
+  type        = string
+  default     = "fhnsdevcollaboraregistry"
+  description = "Name for resources"
+}
+
+variable "location" {
+  type        = string
+  default     = "uksouth"
+  description = "Azure Location of resources"
+}
