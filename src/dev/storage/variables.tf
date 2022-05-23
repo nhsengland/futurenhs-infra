0352b346b37b8e81a4e2f2ac0272a9566bf6688c
RiccardoFarrugia <102971105+RiccardoFarrugia@users.noreply.github.com>
2022-05-23 15:25:58 +0100

    Update variables.tf

--- src/dev/storage/variables.tf
+++ src/dev/storage/variables.tf
@@ -11,3 +11,10 @@ variable key_vault_id { type = string }
 variable log_analytics_workspace_resource_id { type = string }
 
 variable log_storage_account_id { type = string }
+
+variable log_storage_account_id { type = string }
+variable "name" {
+  type        = string
+  default     = "fhnsdevcollaboraregistry"
+  description = "Name for resources"
+}
