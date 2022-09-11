7585a7dd479a9104e91e591176f64ef0bd3ff141
Tim-Hunt <timothy.hunt@madetech.com>
2022-09-11 22:55:46 +0100

    1

--- src/dev/b2c/tenant/main.tf
+++ src/dev/b2c/tenant/main.tf
@@ -1,39 +1,63 @@
+data "azuread_client_config" "current" {}
 
 data "azuread_application_published_app_ids" "well_known" {}
+
 resource "azuread_service_principal" "msgraph" {
-
-use_existing   = true
+  application_id = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
+  use_existing = true
 }
 
 
-resource "azuread_application" "graph_worker" {
-display_name     = var.application_name
-sign_in_audience = "AzureADMyOrg"
+resource "azuread_application" "app-api" {
+  display_name = var.application_name
+  owners       = [data.azuread_client_config.current.object_id]
+  
 
+  api {
+     oauth2_permission_scope {
+      admin_consent_description  = "Allow the application to access example on behalf of the signed-in user."
+      admin_consent_display_name = "Access example"
+      enabled                    = true
+      type                       = "User"
+      user_consent_description   = "Allow the application to access example on your behalf."
+      user_consent_display_name  = "Access example"
+      value                      = "user_impersonation"
+    }
+  }
 
-web {
-redirect_uris = ["${var.application_fqdn}/api/auth/callback/azure-ad-b2c/"]
-}
+  app_role {
+    allowed_member_types = ["User", "Application"]
+    description          = "Admins can manage roles and perform all task actions"
+    display_name         = "Admin"
+    enabled              = true
+    id                   = data.azuread_client_config.current.object_id
+    value                = "application-administrator"
+  }
 
+  web {
+    homepage_url  = var.application_fqdn
+    logout_url    = "${var.application_fqdn}/signout"
+    redirect_uris = ["${var.application_fqdn}/api/auth/callback/azure-ad-b2c/"]
 
-required_resource_access {
-resource_app_id = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
+    implicit_grant {
+      access_token_issuance_enabled = true
+      id_token_issuance_enabled     = true
+    }
+  }
 
+  required_resource_access {
+    resource_app_id = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph # Microsoft Graph
 
-resource_access {
-  id   = azuread_service_principal.msgraph.app_role_ids["Application.ReadWrite.All"]
-  type = "Role"
-}
+   
 
-}
-}
-
-resource "azuread_service_principal" "graph_worker" {
-application_id      = azuread_application.graph_worker.application_id
-}
-
-resource "azuread_app_role_assignment" "graph_worker" {
-app_role_id         = azuread_service_principal.msgraph.app_role_ids["Application.ReadWrite.All"]
-principal_object_id = azuread_service_principal.graph_worker.object_id
-resource_object_id  = azuread_service_principal.msgraph.object_id
-}
+     resource_access {
+          id   = azuread_service_principal.msgraph.app_role_ids["User.Read.All"]
+          type = "Role"
+        }
+    
+        resource_access {
+          id   = azuread_service_principal.msgraph.oauth2_permission_scope_ids["User.ReadWrite"]# User.Read.All
+          type = "Scope"
+        }
+      }
+    }
