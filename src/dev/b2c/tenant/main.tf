0360b143c440eb03e07829ba5c41c006a9a29802
Tim-Hunt <timothy.hunt@madetech.com>
2022-09-11 21:56:48 +0100

    1

--- src/dev/b2c/tenant/main.tf
+++ src/dev/b2c/tenant/main.tf
@@ -1,7 +1,7 @@
 
 data "azuread_application_published_app_ids" "well_known" {}
 resource "azuread_service_principal" "msgraph" {
-application_id = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
+
 use_existing   = true
 }
 
