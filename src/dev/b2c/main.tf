1d2e32fa541cfe38d54856b1af440f458ad90858
Ian King <ian.antking@gmail.com>
2022-11-18 15:28:30 +0000

    concatenate path.module to file key

--- src/dev/b2c/main.tf
+++ src/dev/b2c/main.tf
@@ -27,5 +27,5 @@ resource "azurerm_storage_blob" "b2c_files" {
   storage_account_name   = var.storage_account_name
   storage_container_name = var.storage_container_name
   type                   = "Block"
-  source                 = each.key
+  source                 = "${path.module}/${each.key}"
 }
