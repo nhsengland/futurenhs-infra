d3a556eb2f62fd9574c0e14a826e9c5335f45725
Ian King <ian.antking@gmail.com>
2022-11-18 14:50:20 +0000

    use storage container name

--- src/dev/b2c/main.tf
+++ src/dev/b2c/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_storage_blob" "b2c_files" {
   for_each               = fileset(path.module, "public/*")
   name                   = each.key
   storage_account_name   = var.storage_account_name
-  storage_container_name = azurerm_storage_container.example.name
+  storage_container_name = var.storage_container_name
   type                   = "Block"
   source                 = each.key
 }
