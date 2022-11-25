c4ef967f005ae67bf8c5b75b7298ddc3e306f11c
Ian King <ian.antking@gmail.com>
2022-11-25 10:14:33 +0000

    set blob content types

--- src/dev/b2c/main.tf
+++ src/dev/b2c/main.tf
@@ -21,14 +21,25 @@
 ##   value = azurerm_aadb2c_directory.tenant.tenant_id
 ## }
 
-resource "azurerm_storage_blob" "b2c_files" {
-  for_each               = fileset(path.module, "public/*")
-  name                   = split("/", each.key)[1]
+resource "azurerm_storage_blob" "b2c_html" {
+  for_each               = fileset(path.module, "public/html/*")
+  name                   = split("/", each.key)[2]
   storage_account_name   = var.storage_account_name
-  storage_container_name = "$web"
+  storage_container_name = var.storage_container_name
   type                   = "Block"
-  source_content         = templatefile("${path.module}/${each.key}", { 
-    ENV = var.environment 
+  content_type           = "text/html"
+  source_content = templatefile("${path.module}/${each.key}", {
+    ENV      = var.environment
     HOME_URL = var.environment == "prod" ? "https://collaborate.future.nhs.uk" : "https://collaborate-${var.environment}.future.nhs.uk"
   })
+}
+
+resource "azurerm_storage_blob" "b2c_css" {
+  for_each               = fileset(path.module, "public/css/*")
+  name                   = split("/", each.key)[2]
+  storage_account_name   = var.storage_account_name
+  storage_container_name = var.storage_container_name
+  type                   = "Block"
+  content_type           = "text/css"
+  source                 = "${path.module}/${each.key}"
 }
--- src/dev/b2c/variables.tf
+++ src/dev/b2c/variables.tf
@@ -29,3 +29,7 @@ variable "location" {
 variable "storage_account_name" {
   type = string
 }
+
+variable "storage_container_name" {
+  type = string
+}
